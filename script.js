fea19e9d0a4f8faf6e5dcf69f1bbb8790be6dda5
Brenda Rayane <brenda.rayane.0018@gmail.com>
2024-01-19 12:10:39 -0300

    verificação da categoria escolhida

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-import { categories } from "./arrays.js";
+import { categories, countries, animamls, fruits } from "./arrays.js";
 
 function renderCategories(){
     const containerButtons = document.querySelector(".botoes");
     containerButtons.innerHTML = categories.map(category => {
-      return  `<button class="botao">${category}<button>`
+        return  `<button class="botao" onclick="chooseCategory(this)">${category}</button>`
     }).join('');
+
+    const buttons = document.querySelectorAll(".botao");
+    buttons.forEach( b => {
+        b.addEventListener('click', () => chooseCategory(b))
+    })
 }
 
 renderCategories();
+
+function chooseCategory(element){
+    switch (element.textContent.toLowerCase()){
+        case "países":
+            console.log(element.textContent);
+            break;
+        
+        case "animais":
+            console.log(element.textContent);
+            break;
+        
+        case "frutas":
+            console.log(element.textContent);
+            break;
+
+        default:
+            console.log("Categoria não encontrada");
+    }
+}
