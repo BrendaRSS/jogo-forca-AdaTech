f78d8dccedb29a6f5a5508fed09a96fe25d905e6
Leandro Matos <leandro.matos@live.com>
2024-01-19 23:03:23 -0300

    fix: refeito todo o código do teclado, agora ele usa além dos botões as teclas do teclado.

--- src/js/script.js
+++ src/js/script.js
@@ -24,7 +24,7 @@ function chooseCategory(element) {
             break;
 
         case "animais":
-            drawnWord = animamls[Math.floor(Math.random() * countries.length)];
+            drawnWord = animals[Math.floor(Math.random() * countries.length)];
             break;
 
         case "frutas":
@@ -47,9 +47,67 @@ function chooseCategory(element) {
 function renderWord() {
     const wordArray = drawnWord.split("");
     const divDrawnWord = document.querySelector(".palavra-sorteada");
-    divDrawnWord.innerHTML = wordArray.map(l => {
-        return `<span>__</span>`
+    
+    divDrawnWord.innerHTML = wordArray.map(letter => {
+        if (correctLetters.includes(letter)) {
+            return `<span>${letter}</span>`;
+        } else {
+            return `<span>__</span>`;
+        }
     }).join('');
+}
+
+document.addEventListener('keydown', handleKeyPress);
+
+// Ouvinte de evento para capturar os cliques nos botões do teclado
+const keyboard = document.querySelector(".teclado");
+keyboard.addEventListener('click', handleButtonClick);
+
+function handleKeyPress(event) {
+    const pressedKey = event.key.toLowerCase();
+
+    // Verifica se a tecla pressionada é uma letra do alfabeto
+    if (/^[a-z]$/.test(pressedKey)) {
+        handleKey(pressedKey);
+    }
+}
+
+function handleButtonClick(event) {
+    // Verifica se o clique foi em um botão do teclado
+    if (event.target.tagName === 'BUTTON') {
+        const clickedKey = event.target.id.toLowerCase();
+        handleKey(clickedKey);
+    }
+}
+
+function handleKey(key) {
+    // Desativa a tecla no teclado
+    disableKey(key);
+
+    // Verifica se a letra está correta ou errada
+    if (drawnWord.includes(key)) {
+        correctLetters.push(key);
+    } else {
+        wrongLetters.push(key);
+    }
+
+    // Atualiza a exibição da palavra sorteada
+    renderWord();
+
+    // Verifica se o usuário ganhou ou perdeu
+    checkGameStatus();
+}
+
+function disableKey(key) {
+    // Desativa a tecla no teclado
+    const keyElement = document.getElementById(key);
+    if (keyElement) {
+        keyElement.disabled = true;
+    }
+}
+
+function checkGameStatus() {
+    // Lógica para verificar se o usuário ganhou ou perdeu
 }
 
 // OBS.: Toda vez que o usuário apertar uma letra aconceterá:
